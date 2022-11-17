8e499a30694d301bbc67ded83ab31113036a12f4
guillaumeth <g10@gmx.fr>
2022-11-17 13:02:45 +0100

    api with address generation

--- SERVER/dogepower/api/viewsets.py
+++ SERVER/dogepower/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
+from block_io import BlockIo
 from rest_framework import viewsets
 from rest_framework import mixins
 from .serializers import PowerSupplySerializer
@@ -7,18 +8,30 @@ from .models import PowerSupply
 
 class PowerSupplyViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin,):
     serializer_class = PowerSupplySerializer
-    permission_classes = (IsAuthenticated,)
     http_method_name = ['get']
     queryset = PowerSupply.objects.all()
     
     
-    def get(self, request, pk, address=None):
-        power_supplies_queryset = PowerSupply.objects.all()
-        serializer = PowerSupplySerializer(
-            power_supplies_queryset, many=True)
+    def get(self, request, pk=None, address=None):
         print(pk)
-        print(address)
-        return Response(serializer.data)        	
+        if pk:
+            try:
+                power_supplies_queryset = PowerSupply.objects.filter(address=pk)
+                serializer = PowerSupplySerializer(
+                    power_supplies_queryset, many=True)
+                return Response(serializer.data)
+            except PowerSupply.DoesNotExist:
+                Response({'error': 'address not found'})
+        else:
+            bio = BlockIo('c9ef-3434-8923-ee2b', 'tarticulatio951bretagne', 2)
+            new_address = bio.get_new_address()
+            if new_address and new_address['status'] == "success":
+                new_address_obj = PowerSupply.objects.create(
+                    address=new_address['data']['address'],
+                )
+                return Response({'new_address': new_address_obj.address})
+            else:
+                return Response({'error': 'error getting an address'})
         
         
         
