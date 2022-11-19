cd477c5dc53df5490a3e87a9083ada6c081f939c
guillaumeth <g10@gmx.fr>
2022-11-19 21:38:09 +0100

    cleaned code

--- SERVER/dogepower/api/viewsets.py
+++ SERVER/dogepower/api/viewsets.py
@@ -1,8 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from block_io import BlockIo
 from datetime import timedelta
-from rest_framework import viewsets
-from rest_framework import mixins
+from rest_framework import mixins, views, viewsets
 from .serializers import PowerSupplySerializer
 from rest_framework.response import Response
 from django.utils import timezone
@@ -11,33 +10,36 @@ from .models import PowerSupply
 import decimal
 
 class PowerSupplyViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin,):
+    # Power supply is a singleton created if no address is given in URL
     serializer_class = PowerSupplySerializer
-    http_method_names = ['get', 'post']
+    http_method_names = ['get']
     queryset = PowerSupply.objects.all()
     
     
     def get(self, request, pk=None, address=None):
-        bio = BlockIo('1917-5233-735e-5d5c', 'tarticulatio951bretagne', 2)
+        bio = BlockIo('xxxx-xxxx-xxxx-xxxx', 'apipassword', 2)
         if pk:
             try:
                 power_supplies_queryset = PowerSupply.objects.filter(address=pk)
                 serializer = PowerSupplySerializer(
                     power_supplies_queryset, many=True)
-
                 last_power_supply = power_supplies_queryset.filter(valid=True).last()
-                if last_power_supply:
-                    wallet_amount = decimal.Decimal(bio.get_address_balance(addresses='%s' % last_power_supply.address)['data']['available_balance'])
-                    price = last_power_supply.minute_price * 60 * last_power_supply.initial_time if last_power_supply.time_unit == 'H' \
-                        else last_power_supply.minute_price * 60 * 12 * last_power_supply.initial_time
-                    if wallet_amount > last_power_supply.last_device_amount and wallet_amount > last_power_supply.last_device_amount / last_power_supply.minute_price:
-                        time_paid_sec = int(wallet_amount - last_power_supply.last_device_amount / last_power_supply.minute_price) * 60
-                        if last_power_supply.payment_expires <= timezone.now():
-                            last_power_supply.payment_expires = timezone.now() + timedelta(seconds=time_paid_sec)
-                        else:
-                            last_power_supply.payment_expires = last_power_supply.payment_expires + timedelta(seconds=time_paid_sec)
-                        last_power_supply.last_device_amount += wallet_amount
-                        last_power_supply.paid = True
-                        last_power_supply.save()
+                # wallet_amount is the amount on the wallet, it's compared to the last amount the device had
+                wallet_amount = decimal.Decimal(bio.get_address_balance(addresses='%s' % last_power_supply.address)['data']['available_balance'])
+                # be careful Doge Power don't give change !
+                if wallet_amount > last_power_supply.last_device_amount:
+                    # the time we got from payment is calculated and added in the 'payment_expires' attribute
+                    time_paid_sec = int(wallet_amount - last_power_supply.last_device_amount / last_power_supply.minute_price) * 60
+                    if last_power_supply.payment_expires <= timezone.now():
+                        last_power_supply.payment_expires = timezone.now() + timedelta(seconds=time_paid_sec)
+                    else:
+                        last_power_supply.payment_expires = last_power_supply.payment_expires + timedelta(seconds=time_paid_sec)
+                    # we save the new amount on the device
+                    last_power_supply.last_device_amount += wallet_amount
+                    # 'paid' and 'valid' attributes are not used yet
+                    last_power_supply.paid = True
+                    last_power_supply.save()
+                # remaining time is converted to seconds
                 sec_left = last_power_supply.payment_expires - timezone.now() if last_power_supply and last_power_supply.payment_expires > timezone.now() else None
                 return Response({
                     'data': serializer.data,
@@ -47,6 +49,7 @@ class PowerSupplyViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixin
             except PowerSupply.DoesNotExist:
                 Response({'error': 'address not found'})
         else:
+            # generation of a new address and a PowerSupply object singleton
             new_address = bio.get_new_address()
             if new_address and new_address['status'] == "success":
                 new_address_obj = PowerSupply.objects.create(
@@ -56,16 +59,15 @@ class PowerSupplyViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixin
             else:
                 return Response({'error': 'error getting an address'})
 
-    def post(self, request, pk=None, address=None, test=None):
-        new_power_supply = PowerSupply.objects.create(address=pk)
-        power_supplies_queryset = PowerSupply.objects.filter(address=pk)
-        last_power_supply = power_supplies_queryset.filter(valid=True).last()
-        if last_power_supply:
-            new_power_supply.last_device_amount = last_power_supply.last_device_amount
-            new_power_supply.save()
-        serializer = PowerSupplySerializer(
-            power_supplies_queryset, many=True)
-        return Response(serializer.data)
+class AddressView(views.APIView):
+    # thiis view generates a list of all addressesavailable on the account
+    serializer_class = PowerSupplySerializer
+    http_method_names = ['get']
+    queryset = PowerSupply.objects.all()
+
+    def get(self, request):
+        addresses_list = [ps.address for ps in PowerSupply.objects.all()]
+        return Response(addresses_list)
         
         
 
